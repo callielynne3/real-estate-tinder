e6cb756bdaebbefd1665889a12838d498d57e17d
cclazarou <candacelazarou@gmail.com>
2017-02-07 09:16:29 -0800

    Add styling to post rental preview form

--- client/app/bundles/rentals/components/RentalForm.jsx
+++ client/app/bundles/rentals/components/RentalForm.jsx
@@ -14,63 +14,67 @@ export default class RentalForm extends React.Component {
     return (
 
       <div className="RentalFormContainer">
-        <form action="/preview" method="post" className="ui form">
+        <div className="ui container">
 
-            <div className="required field">
-              <label htmlFor="rental[title]">Title: </label>
-              <input type="text" name="rental[title]" />
-            </div>
+            <form action="/preview" method="post" className="ui big form">
 
-          <div className="fields">
-            <div className="required eight wide field">
-              <label htmlFor="rental[address]">Address: </label>
-              <input type="text" name="rental[address]" />
-            </div>
+                <div className="required field">
+                  <label htmlFor="rental[title]">Title: </label>
+                  <input type="text" name="rental[title]" />
+                </div>
 
-            <div className="three wide field">
-              <label htmlFor="rental[unit]">Unit: </label>
-              <input type="text" name="rental[unit]" />
-            </div>
-          </div>
+              <div className="fields">
+                <div className="required eight wide field">
+                  <label htmlFor="rental[address]">Address: </label>
+                  <input type="text" name="rental[address]" />
+                </div>
 
-            <div className="required field">
-              <label htmlFor="rental[price]">Price: </label>
-              <input type="number" name="rental[price]" />
-            </div>
+                <div className="three wide field">
+                  <label htmlFor="rental[unit]">Unit: </label>
+                  <input type="text" name="rental[unit]" />
+                </div>
+              </div>
 
-          <div className="required fields">
-            <div className="field">
-              <label htmlFor="rental[beds]">Bedrooms: </label>
-              <input type="number" name="rental[beds]" />
-            </div>
+                <div className="required field">
+                  <label htmlFor="rental[price]">Price: </label>
+                  <input type="number" name="rental[price]" min="0" />
+                </div>
 
-            <div className="required field">
-              <label htmlFor="rental[baths]">Bathrooms: </label>
-              <input type="number" name="rental[baths]" />
-            </div>
-          </div>
+              <div className="required fields">
+                <div className="field">
+                  <label htmlFor="rental[beds]">Bedrooms: </label>
+                  <input type="number" name="rental[beds]" min="0"/>
+                </div>
 
-            <div className="ui checkbox">
-                <input type="Checkbox"  tabindex="0" name="rental[pets]" class="hidden"/>
-                <label htmlFor="rental[pets]">Pet Friendly</label>
-            </div>
+                <div className="required field">
+                  <label htmlFor="rental[baths]">Bathrooms: </label>
+                  <input type="number" name="rental[baths]" min="0" />
+                </div>
+              </div>
 
-            <div className="field">
-              <label htmlFor="rental[parking]">Parking: </label>
-              <input type="text" name="rental[parking]" defaultValue="Street parking, garage, etc." />
-            </div>
+                <div className="ui checkbox" id="checkbox">
+                    <input type="Checkbox"  tabindex="0" name="rental[pets]" class="hidden"/>
+                    <label htmlFor="rental[pets]">Pet Friendly</label>
+                </div>
 
-            <div className="field">
-              <label htmlFor="rental[description]">Rental Description: </label>
-              <textarea name="rental[description]" defaultValue="Describe your rental in detail."></textarea>
-            </div>
+                <div className="field">
+                  <label htmlFor="rental[parking]">Parking: </label>
+                  <input type="text" name="rental[parking]" defaultValue="Street parking, garage, etc." />
+                </div>
 
-            <div className="field">
-              <input type="submit" value="List My Rental" className="ui button"/>
-            </div>
+                <div className="field">
+                  <label htmlFor="rental[description]">Rental Description: </label>
+                  <textarea name="rental[description]" defaultValue="Describe your rental in detail."></textarea>
+                </div>
 
-        </form>
+                <div className="ui one column centered grid">
+                  <div className="field form-submit-button">
+                    <input type="submit" value="List My Rental" className="ui big button"/>
+                  </div>
+                </div>
+            </form>
 
+        </div>
       </div>
     )
 
