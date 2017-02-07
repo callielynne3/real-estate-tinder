f6b959f656ed96bd41a29f5030e9c4183ed4319a
ejoreo <ejorear@gmail.com>
2017-02-06 17:20:47 -0800

    Position rental queries on welcome page

--- client/app/bundles/welcome/components/rentalQueries.jsx
+++ client/app/bundles/welcome/components/rentalQueries.jsx
@@ -12,21 +12,19 @@ export default class rentalQueries extends React.Component {
   render() {
 
     return(
-      <div className="rentalQueries">
-        <p>
-          <select id="propertyType">
-            <option value="apartment">Studio/Apartment</option>
-            <option value="room">Room</option>
-          </select>
-        </p>
-        <p>
-          <select id="priceRange">
-            <option value="lowest">less than $1,000</option>
-            <option value="low">$1,000-$2,000</option>
-            <option value="medium">$3,000-$4,000</option>
-            <option value="high">more than $4,000</option>
-          </select>
-        </p>
+      <div className="rentalQueriesContainer">
+        <div className="rentalQueries">
+            <select id="propertyType">
+              <option value="apartment">Studio/Apartment</option>
+              <option value="room">Room</option>
+            </select>
+            <select id="priceRange">
+              <option value="lowest">less than $1,000</option>
+              <option value="low">$1,000-$2,000</option>
+              <option value="medium">$3,000-$4,000</option>
+              <option value="high">more than $4,000</option>
+            </select>
+        </div>
       </div>
     )
   }
