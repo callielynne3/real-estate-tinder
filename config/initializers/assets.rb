432a3b1b7b8b4ce6887cc019e2c03dfb820b00bf
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2017-02-08 10:32:10 -0800

    :bug: Asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -19,6 +19,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Add folder with webpack generated assets to assets.paths
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "webpack")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "stylesheets", "dist")
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|woff2|ttf)$/
 
 Rails.application.config.assets.precompile << Proc.new { |path|
