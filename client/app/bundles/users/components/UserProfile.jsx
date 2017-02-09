b6a497df947f921bb260da6a321879e4d51ccd9e
cclazarou <candacelazarou@gmail.com>
2017-02-09 11:28:35 -0800

    Style image on profile page

--- client/app/bundles/users/components/UserProfile.jsx
+++ client/app/bundles/users/components/UserProfile.jsx
@@ -6,12 +6,12 @@ class UserProfile extends React.Component {
   }
 
   renderPicture() {
-   return <img src={this.props.user.picture.url} />
+   return <img className="ui medium rounded centered image" src={this.props.user.picture.url} />
   }
 
   renderFavorites() {
     if (this.props.votes.length === 0) {
-      return 
+      return
         <div>
           <button class="ui button"><a href={this.props.homeLink}>Find Your New Home</a></button>
         </div>;
@@ -23,7 +23,7 @@ class UserProfile extends React.Component {
 
   renderRentals() {
     if (this.props.rentals.length === 0) {
-      return 
+      return
         <div>
           <button class="ui button"><a href={this.props.newRentalLink}>Post a New Listing</a></button>
         </div>;
