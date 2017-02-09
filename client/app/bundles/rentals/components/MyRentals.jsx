e820d6685949f45d590e08398b59383dadcf4d2f
= <=>
2017-02-08 18:57:01 -0800

    :white_check_mark: Make cards stackable grids

--- client/app/bundles/rentals/components/MyRentals.jsx
+++ client/app/bundles/rentals/components/MyRentals.jsx
@@ -9,33 +9,33 @@ export default class MyRentals extends React.Component {
     return this.props.rentals.map((rental) => {
 
       return (
-        <div class="column">
-          <div key={rental.id} className="">
-            <div className="ui card">
-              <div className="image">
-                <img src="http://lorempixel.com/400/200/" />
-              </div>
-              <div className="content">
-                <div className="header">{rental.title}</div>
-                <div className="meta">
-                  <a>{rental.address}</a>
+        <div className="five wide column">
+            <div key={rental.id} className="">
+              <div className="ui card">
+                <div className="image">
+                  <img src="http://lorempixel.com/400/200/" />
                 </div>
-                <div className="description">
-                  {rental.property_type}
+                <div className="content">
+                  <div className="header">{rental.title}</div>
+                  <div className="meta">
+                    <a>{rental.address}</a>
+                  </div>
+                  <div className="description">
+                    {rental.property_type}
+                  </div>
                 </div>
-              </div>
-              <div className="extra content">
-                <span className="right floated">
-                  Posted by {rental.user_id}
-                </span>
-                <span>
-                  <i className="user icon"></i>
-                  Posted on {rental.created_at}
-                </span>
+                <div className="extra content">
+                  <span className="right floated">
+                    Posted by {rental.user_id}
+                  </span>
+                  <span>
+                    <i className="user icon"></i>
+                    Posted on {rental.created_at}
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-        </div>  
+          </div>  
       );
     });
   }
@@ -44,7 +44,9 @@ export default class MyRentals extends React.Component {
     return (
        <div>
         <h1>My Rentals</h1>
-        {this.renderRentals()}
+        <div className="ui grid container stackable">
+          {this.renderRentals()}
+        </div>
       </div>
     );
   }
