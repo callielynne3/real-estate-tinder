65126773c9247e6417c166dab6d0af8012f85cc9
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2017-02-08 20:32:45 -0800

    :earth_africa: Lat + Long

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207235814) do
+ActiveRecord::Schema.define(version: 20170209033954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20170207235814) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.json     "pictures"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_rentals_on_user_id", using: :btree
   end
 
